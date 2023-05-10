cfbfc8a9eab2914af9e638a218930f53a102a275
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-05-10 18:43:58 +0300

    Add files via upload

--- HotDog or Not!/HotDog or Not!/ViewController.swift
+++ HotDog or Not!/HotDog or Not!/ViewController.swift
@@ -31,10 +31,56 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             
             imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                
+                fatalError("Could not convert to UIImage into CIImage")
+            }
+            // detect fonksiyonunu çağırarak ciimage i aktarıyoruz
+            detectImage(image:ciImage )
         }
+        
+       
         
         imagePicker.dismiss(animated: true,completion: nil)
             
+    }
+    
+    func detectImage (image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {
+                
+                fatalError("Model failed to process Image.")
+            }
+            
+            if let firstResult = results.first{
+                print(firstResult)
+                if firstResult.identifier.contains("hotdog") {
+                    
+                    print("i found The hotdog")
+                    self.navigationController?.navigationBar.topItem!.title = "HotDog!"
+                }else {
+                    
+                    print("i didnt find the hot dog")
+                    self.navigationController?.navigationBar.topItem!.title = "Not Hotdog!"
+                }
+            }
+            
+//            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
